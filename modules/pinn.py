02ddef6aff4b6d3606faca6d86c4b04a4a779d02
joon-stack <zuni1218@gmail.com>
2021-10-01 21:17:08 +0900

    Update continous beam solution

--- modules/pinn.py
+++ modules/pinn.py
@@ -9,22 +9,22 @@ class PINN(nn.Module):
 
         self.id = id
 
-        self.hidden_layer1      = nn.Linear(1, 40)
-        self.hidden_layer2      = nn.Linear(40, 40)
-        self.hidden_layer3      = nn.Linear(40, 40)
-        self.hidden_layer4      = nn.Linear(40, 40)
-        self.hidden_layer5      = nn.Linear(40, 40)
-        self.output_layer       = nn.Linear(40, 1)
+        self.hidden_layer1      = nn.Linear(1, 20)
+        self.hidden_layer2      = nn.Linear(20, 20)
+        self.hidden_layer3      = nn.Linear(20, 20)
+        # self.hidden_layer4      = nn.Linear(20, 20)
+        # self.hidden_layer5      = nn.Linear(20, 20)
+        self.output_layer       = nn.Linear(20, 1)
 
     def forward(self, x):
         input_data     = x
-        act_func       = nn.Sigmoid()
+        act_func       = nn.Tanh()
         a_layer1       = act_func(self.hidden_layer1(input_data))
         a_layer2       = act_func(self.hidden_layer2(a_layer1))
         a_layer3       = act_func(self.hidden_layer3(a_layer2))
-        a_layer4       = act_func(self.hidden_layer3(a_layer3))
-        a_layer5       = act_func(self.hidden_layer3(a_layer4))
-        out            = self.output_layer(a_layer5)
+        # a_layer4       = act_func(self.hidden_layer3(a_layer3))
+        # a_layer5       = act_func(self.hidden_layer3(a_layer4))
+        out            = self.output_layer(a_layer3)
 
         # out *= window(input_data, 0.4, 0.6, i=self.id)
 
@@ -34,12 +34,12 @@ def sigmoid(x):
     return 1 / (1 + torch.exp(-x))
 
 def window(x, a, b, i):
-    act_func = nn.Sigmoid()
+    act_func = nn.ReLU()
     # return sigmoid((x - a) / 0.01) * sigmoid((b - x) / 0.01)
     if i == 1:
-        return act_func((x - a) / 0.1)
+        return act_func((x - a) / 100)
     elif i == 0:
-        return act_func((b - x) / 0.1)
+        return act_func((b - x) / 100)
 
 # def window_test():
 #     x_test = np.arange(100) / 50
--- train.py
+++ train.py
@@ -24,6 +24,15 @@ class CombinedPINN(nn.Module):
         
         out = self.modelA(x) * window(x, 0.4, 0.6, i=0) + self.modelB(x) * window(x, 0.4, 0.6, i=1)
         return out
+
+    def out_A(self, x):
+        out = self.modelA(x) * window(x, 0.4, 0.6, i=0)
+        return out
+    
+    def out_B(self, x):
+        out = self.modelB(x) * window(x, 0.4, 0.6, i=1)
+        return out
+    
     
 
 
@@ -47,35 +56,54 @@ def train():
    # Define CombinedPINN
     model = CombinedPINN(2)
     optims = []
-    optims.append(torch.optim.Adam(model.modelA.parameters(), lr=0.01))
-    optims.append(torch.optim.Adam(model.modelB.parameters(), lr=0.01))
+    optims.append(torch.optim.Adam(model.modelA.parameters(), lr=0.001))
+    optims.append(torch.optim.Adam(model.modelB.parameters(), lr=0.001))
 
-    x_b, u_b = make_training_boundary_data(b_size, x=0.0, u=0.0)
+    x_b, u_b = make_training_boundary_data(b_size // 3, x=0.0, u=0.0)
+    x_b_2, u_b_2 = make_training_boundary_data(b_size // 3, x=0.5, u=0.0)
+    x_b_3, u_b_3 = make_training_boundary_data(b_size // 3, x=1.0, u=0.0)
     x_f, u_f = make_training_collocation_data(f_size // 2, x_lb=0.0, x_rb=0.6)
     x_f_2, u_f_2 = make_training_collocation_data(f_size // 2, x_lb=0.4, x_rb=1.0)
 
 
     loss_save = np.inf
     
+    loss_b_plt = [[] for _ in range(element_no)]
+    loss_f_plt = [[] for _ in range(element_no)]
+    loss_plt   = [[] for _ in range(element_no)]
+
+
     for epoch in range(epochs):
         for i in range(element_no):
             optim = optims[i]
             optim.zero_grad()
-            loss_b = 0.0
-            loss_f = 0.0
+            loss_b = torch.zeros(1)
+            loss_f = torch.zeros(1)
             loss_func = nn.MSELoss()
 
-            loss_sum = 0.0
+            loss_sum = torch.zeros(1)
 
             if i == 0:
-                loss_b += loss_func(model(x_b), u_b)
-                loss_f += loss_func(calc_deriv(x_f, model(x_f), 1) - torch.cos(w * x_f), u_f)
+                loss_b += loss_func(model(x_b), u_b) * 1000
+                loss_b += loss_func(model(x_b_2), u_b_2) * 1000
+                loss_b += loss_func(calc_deriv(x_b, model(x_b), 2), u_b) * 1000
+                loss_b += loss_func(calc_deriv(x_b_2, model(x_b_2), 1), u_b_2) * 1000
+                loss_f += loss_func(calc_deriv(x_f, model(x_f), 4) - 1, u_f)
             elif i == 1:
-                loss_f += loss_func(calc_deriv(x_f_2, model(x_f_2), 1) - torch.cos(w * x_f_2), u_f_2)
+                loss_b += loss_func(model(x_b_3), u_b_3) * 1000
+                loss_b += loss_func(model(x_b_2), u_b_2) * 1000
+                loss_b += loss_func(calc_deriv(x_b_3, model(x_b_3), 2), u_b_3) * 1000
+                loss_b += loss_func(calc_deriv(x_b_2, model(x_b_2), 1), u_b_2) * 1000
+                loss_f += loss_func(calc_deriv(x_f_2, model(x_f_2), 4) - 1, u_f_2)
             
             loss = loss_f + loss_b
             loss_sum += loss
 
+            loss_b_plt[i].append(loss_b.item())
+            loss_f_plt[i].append(loss_f.item())
+            loss_plt[i].append(loss.item())
+
+            
             loss.backward()
 
             optim.step()
@@ -83,7 +111,10 @@ def train():
             with torch.no_grad():
                 model.eval()
             
-            print("Epoch: {0} | LOSS: {1:.5f}".format(epoch+1, loss))
+            print("Epoch: {0} | LOSS: {1:.5f}".format(epoch+1, loss.item()))
+
+            if epoch % 50 == 1:
+                draw_convergence(epoch + 1, loss_b_plt[i], loss_f_plt[i], loss_plt[i], i)
 
         if loss_sum < loss_save:
             loss_save = loss_sum
@@ -95,12 +126,22 @@ def train():
 
         if epoch % 50 == 1:
             draw()
+            
 
     print("DONE")
 
 def exact(x):
     return torch.sin(20 * x) / 20
 
+def draw_convergence(epoch, loss_b, loss_f, loss, id):
+    plt.cla()
+    x = np.arange(epoch)
+
+    plt.plot(x, np.array(loss_b), label='Loss_B')
+    plt.plot(x, np.array(loss_f), label='Loss_F')
+    plt.plot(x, np.array(loss), label='Loss')
+    plt.legend()
+    plt.savefig('./figures/convergence_{}.png'.format(id))
 
 def draw():
     model = CombinedPINN(2)
@@ -119,16 +160,34 @@ def draw():
     ex = exact(x_test).detach().numpy()
 
     plt.plot(x_test, pred, 'b', label='CPINN')
-    plt.plot(x_test, ex, 'r--', label='Exact')
+    # plt.plot(x_test, ex, 'r--', label='Exact')
     plt.legend()
     plt.savefig('./figures/test.png')
 
     plt.cla()
-    modelA
-    
+    plt.plot(x_test, model.out_A(x_test).detach().numpy(), 'b', label='A')
+    plt.plot(x_test, model.out_B(x_test).detach().numpy(), 'r--', label='B')
+    plt.legend()
+    plt.savefig('./figures/separate.png')
 
+    plt.cla()
+    plt.plot(x_test, window(x_test, 0.4, 0.6, i=0), 'b', label='A')
+    plt.plot(x_test, window(x_test, 0.4, 0.6, i=1), 'r--', label='B')
+    plt.legend()
+    plt.savefig('./figures/window.png')
+
+def calc_deriv_test():
+    x = torch.from_numpy((np.arange(1000)) / 1000).type(torch.FloatTensor)
+    x.requires_grad = True
+    y = x * x * x * x 
+    y_x = calc_deriv(x, window(x, 0.4, 0.6, i=0), 4).detach().numpy()
+
+    plt.cla()
+    plt.plot(x.detach().numpy(), y_x, 'b')
+    plt.savefig('./figures/deriv_test.png')
     
 def main():
+    # calc_deriv_test()
     train()
     draw()
 
